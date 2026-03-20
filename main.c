1555f96c14dd2906af2f4341ce133d25403ac356
Leo Moe <moetheleo@gmail.com>
2026-03-20 16:03:48 +0100

    now we can get array for initialization with ./run

--- main.c
+++ main.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
+#include <stdbool.h>
 
+int main(int intc, char** args){
+    if(intc!=2){ printf("you need different amount of arguments!\n");}
+    char * c=args[1];
+    int index=0;
+    printf("{");
+    while(true){
+        char toprint=c[index];
+        if(toprint==0){break;}
 
-int main(){
-
+        switch (toprint) {
+            case '>':
+                printf("0");
+                break;
+            case '<':
+                printf("1");
+                break;
+            case '+':
+                printf("2");
+                break;
+            case '-':
+                    printf("3");
+                    break;
+            case '.':
+                    printf("4");
+                    break;
+            case ',': 
+                    printf("5");
+                    break;
+            case '[':
+                    printf("6");
+                    break;
+            case ']':
+                    printf("7");
+                    break;
+            default:
+        printf("Default case is Matched.");
+        break;
+        
+            }
+        printf(",");
+        index++;
+    }
+    printf("}\n");
+    
 
 
     return 0;
