f476f21e25cf525e7e22851726fd15a510901940
retr0sity <ioannis.karkalas@gmail.com>
2026-03-20 18:41:01 +0100

    Changed the C file to make a file we can feed to actsim, and also made a gitignore for the compiled and the file it made

--- main.c
+++ main.c
@@ -2,50 +2,28 @@
 #include <stdbool.h>
 
 int main(int intc, char** args){
-    if(intc!=2){ printf("you need different amount of arguments!\n");}
-    char * c=args[1];
-    int index=0;
-    printf("{");
+    if(intc!=2){ printf("you need different amount of arguments!\n"); return 1; }
+    
+    FILE* f = fopen("_infile_.0", "w");
+    char* c = args[1];
+    int index = 0;
+    
     while(true){
-        char toprint=c[index];
-        if(toprint==0){break;}
-
-        switch (toprint) {
-            case '>':
-                printf("0");
-                break;
-            case '<':
-                printf("1");
-                break;
-            case '+':
-                printf("2");
-                break;
-            case '-':
-                    printf("3");
-                    break;
-            case '.':
-                    printf("4");
-                    break;
-            case ',': 
-                    printf("5");
-                    break;
-            case '[':
-                    printf("6");
-                    break;
-            case ']':
-                    printf("7");
-                    break;
-            default:
-        printf("Default case is Matched.");
-        break;
-        
-            }
-        printf(",");
+        char toprint = c[index];
+        if(toprint == 0) break;
+        switch(toprint){
+            case '>': fprintf(f, "0 "); break;
+            case '<': fprintf(f, "1 "); break;
+            case '+': fprintf(f, "2 "); break;
+            case '-': fprintf(f, "3 "); break;
+            case '.': fprintf(f, "4 "); break;
+            case ',': fprintf(f, "5 "); break;
+            case '[': fprintf(f, "6 "); break;
+            case ']': fprintf(f, "7 "); break;
+        }
         index++;
     }
-    printf("}\n");
     
-
-
+    fclose(f);
     return 0;
 }
