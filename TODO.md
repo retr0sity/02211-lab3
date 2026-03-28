8cf9545c8f2f3998374e8db2ee949fe6c3537093
Leo Moe <moetheleo@gmail.com>
2026-03-28 16:50:22 +0100

    now the add of two values from input works

---
+++ TODO.md
@@ -0,0 +1,2 @@
+# I am going to write the last few lines of the CPU and then run a few tests. 
+This whole project may be almost done, I am unsure. 
---
+++ testCPU.sh
@@ -0,0 +1,5 @@
+export ACT_HOME=$HOME/act-local
+export PATH=$PATH:$ACT_HOME/bin
+aflat -Tsky130l IM.act > IM.pr
+
+actsim -Wlang_subst:off testCPU.act test
